f55c0d2686839d9615498787a932c918d363898e
Sahid-Morales-5500 <sahidm2023@gmail.com>
2026-01-05 23:39:16 -0500

    DataBase uploaded, pipenv folders and git ignore

---
+++ database.py
@@ -0,0 +1,17 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host = os.getenv('DB_HOST'),
+    port =  os.getenv('DB_PORT'),
+    user = os.getenv('DB_USER'),
+    password =  os.getenv('DB_PASSWORD'),
+    dbname =  os.getenv('DB_NAME')
+)
+
+cursor = conn.cursor()
+data = cursor.fetchall()
+print(data)
