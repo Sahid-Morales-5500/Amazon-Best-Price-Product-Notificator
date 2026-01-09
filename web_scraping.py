4edcf4c6d7507add87739c2a5432a0ab7553204d
Sahid-Morales-5500 <sahidm2023@gmail.com>
2026-01-08 23:14:33 -0500

    Funcion Web Scrapping Actualizada

--- web_scraping.py
+++ web_scraping.py
@@ -1,53 +1,51 @@
-import requests
 from bs4 import BeautifulSoup
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-#le puse para que busque productos y asii se modifica la url.
+# 1 - Pedir Producto
 producto = input("ingresa el producto que deseas buscar en Amazon y presiona Enter: ")
-
 url = f"https://www.amazon.es/s?k={producto.replace(' ', '+')}"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "es-ES,es;q=0.9",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Referer": "https://www.google.com/",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1"
-}
+option = Options()
+option.add_argument('--start-maximized')
+option.add_argument('--disable-blink-features=AutomationControlled')
+
+# Driver:
+driver = webdriver.Chrome(
+    service = Service(ChromeDriverManager().install())
+)
+
 
 try:
-    time.sleep(2)
-    respuesta = requests.get(url, headers=headers)
+    # Abrir pagina:
+    driver.get(url)
+    time.sleep(4)
 
-    if respuesta.status_code == 503:
-        print("Error 503: Bloqueo de Amazon. Cambia tu IP.")
-        
-    elif respuesta.status_code == 200:
-        if "captcha" in respuesta.text.lower():
-            print("Error: Amazon pide resolver Captcha.")
-        else:
-            sopa = BeautifulSoup(respuesta.text, 'html.parser')
-            resultados = sopa.find_all('div', {"data-component-type": "s-search-result"})
+    # Renderizar HTML:
+    html = driver.page_source
 
-            print(f"Encontrados: {len(resultados)}")
+    # Mirar Precios:
+    sopa = BeautifulSoup(html, 'html.parser')
+    resultados = sopa.find_all('div', {'data-component-type': 's-search-result'})
+    print(f'Encontrados: {len(resultados)}')
 
-            for producto in resultados:
-                titulo_tag = producto.find('h2')
-                precio_tag = producto.find('span', class_='a-price-whole')
+    for item in resultados:
+        titulo_tag = item.find('h2')
+        precio_tag = item.find('span', class_ = 'a-price-whole')
 
-                if titulo_tag:
-                    titulo = titulo_tag.text.strip()
-                    precio = precio_tag.text.strip() if precio_tag else "Sin precio"
-                    
-                    print(f"{titulo[:60]}... | {precio} euros")
+        if titulo_tag:
+            titulo = titulo_tag.text.strip()
+            precio = precio_tag.text.strip() if precio_tag else 'Sin Precio'
 
-    else:
-        print(f"Error desconocido: {respuesta.status_code}")
+            print(f'{titulo[:60]}... | {precio} €')
+
+
 
 except Exception as e:
     print(f"Error técnico: {e}")
+
+finally:
+    driver.quit()
